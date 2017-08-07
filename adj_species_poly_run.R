7d85990067d43da9c81953d97528c6d91762a013
Sara Williams <sarawil@gmail.com>
2017-08-07 17:13:42 -0600

    Renamed to desginate elevation
    Renamed and adjusted function script to specify that it is only adjusting polygon range according to elevation constraints.

--- adj_species_poly_run.R
+++ adj_species_poly_run.R
@@ -19,23 +19,23 @@ load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/l
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/for_cov_c.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sabah_d.Rdata")
 
-#  Source raster_to_polygon function and adj_species_poly function
+#  Source raster_to_polygon function and adj_species_elev_poly function
 source("C:/Users/saraw/Documents/SEARRP/scripts/raster_to_polygon_fun.R")
 source("C:/Users/saraw/Documents/SEARRP/scripts/adj_species_poly_fun.R")
 
 #  Run function to adjust spp ranges based on elevational constraints
 adj_spp_poly_elev_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_mammals_threatened.csv",
 										sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_mammals_threat.rds",
-										new_spp_sp = "mammals.shp", 
-										new_spp_sf = "new_mammal_ranges_w_dat.rds")
+										new_spp_sp = "mammals.shp") #, 
+										#new_spp_sf = "new_mammal_elev_w_dat.rds")
 ####  NOTE: the shapefile from the IUCN spatial data for Melogale everetti is incorrect.
 ####   Must leave this spp out for the loop to work. It has been removed from
 ####   the larger data files but saved on it's own CSV file.							
 
 adj_spp_poly_elev_fund(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_amphibians_threatened.csv",
 											sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_amphs_threat.rds",
-											new_spp_sp = "amphibians.shp", 
-											new_spp_sf = "new_amphibian_ranges_w_dat.rds")
+											new_spp_sp = "amphibians.shp") #, 
+											#new_spp_sf = "new_amphibian_elev_w_dat.rds")
 ####  NOTE: the shapefile from the IUCN spatial data for Limnonectes ibanorum, Limnonectes rhacodus, 
 ####   Rhacophorus fasciatus, andPelophryne signata shows that these species ranges barely overlap Sabah 
 ####   and thus does not have any part that falls within the elevational constraints. Must leave this spp out 
@@ -43,8 +43,8 @@ adj_spp_poly_elev_fund(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_da
 
 adj_spp_poly_elev_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_birds_threatened.csv",
 											sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_birds_threat.rds",
-											new_spp_sp = "birds.shp", 
-											new_spp_sf = "new_bird_ranges_w_dat.rds")
+											new_spp_sp = "birds.shp") #, 
+											#new_spp_sf = "new_bird_elev_w_dat.rds")
 
 
 
