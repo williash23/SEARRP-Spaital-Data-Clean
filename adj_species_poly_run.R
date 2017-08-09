7e06723f59448ce73a092f8d7d1f1be2ff637d5d
Sara Williams <sarawil@gmail.com>
2017-08-09 11:22:37 -0600

    Added water adjustment function
    Added adjustment of spp range polygons by water dependency constraints (including both rivers and coast line) along with generation of static buffer around water features.

--- adj_species_poly_run.R
+++ adj_species_poly_run.R
@@ -14,21 +14,22 @@ setwd("C:/Users/saraw/Documents/SEARRP")
 
 #  Load environmental spatial data files created in spat_dat_exp.R script
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/elev_250m_c.Rdata")
-load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/hydro_vec_b.Rdata")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/hydro_vec_c.Rdata")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/coast_str.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/log_rds_c.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/for_cov_c.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sabah_d.Rdata")
 
 #  Source raster_to_polygon function and adj_species_elev_poly function
 source("C:/Users/saraw/Documents/SEARRP/scripts/raster_to_polygon_fun.R")
-source("C:/Users/saraw/Documents/SEARRP/scripts/adj_species_poly_fun.R")
-
+source("C:/Users/saraw/Documents/SEARRP/scripts/adj_species_poly_elev_fun.R")
+source("C:/Users/saraw/Documents/SEARRP/scripts/adj_species_poly_water_fun.R")
 
 
 #  Run function to adjust spp ranges based on elevational constraints
 adj_spp_poly_elev_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_mammals_threatened.csv",
 	sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_mammals_threat.rds",
-	new_spp_sp = "mammals.shp") #, 
+	new_spp_sp = "mammals_elev.shp") #, 
 	#new_spp_sf = "new_mammal_elev_w_dat.rds")
 ####  NOTE: the shapefile from the IUCN spatial data for Melogale everetti is incorrect.
 ####   Must leave this spp out for the loop to work. It has been removed from
@@ -36,7 +37,7 @@ adj_spp_poly_elev_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_dat
 
 adj_spp_poly_elev_fund(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_amphibians_threatened.csv",
 	sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_amphs_threat.rds",
-	new_spp_sp = "amphibians.shp") #, 
+	new_spp_sp = "amphibians_elev.shp") #, 
 	#new_spp_sf = "new_amphibian_elev_w_dat.rds")
 ####  NOTE: the shapefile from the IUCN spatial data for Limnonectes ibanorum, Limnonectes rhacodus, 
 ####   Rhacophorus fasciatus, andPelophryne signata shows that these species ranges barely overlap Sabah 
@@ -45,15 +46,22 @@ adj_spp_poly_elev_fund(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_da
 
 adj_spp_poly_elev_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_birds_threatened.csv",
 	sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_birds_threat.rds",
-	new_spp_sp = "birds.shp") #, 
+	new_spp_sp = "birds_elev.shp") #, 
 	#new_spp_sf = "new_bird_elev_w_dat.rds")
 
 
 
 #  Run function to adjust spp ranges based on water dependency constraints.
-#   Convert water feature sf data to sp format for all runs
-hydro_vec_b_sp <- as(hydro_vec_b, "Spatial")	
+#   First, create buffer of desired size around all water (rivers and coastline)
+rivers <- st_as_sf(hydro_vec_c)
+rivers_b <- st_buffer(rivers, 1000)
+coast_b <- st_buffer(coast_str, 990) # coast line already includes a 10m buffer from coast line creation
+all_water <- st_union(rivers_b, coast_b)
 
+adj_spp_poly_water_fun(dat = "C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_mammals_threatened_elev_water.csv",
+	sf = "C:/Users/saraw/Documents/SEARRP/processed_spat_data/sf_mammals_threat.rds",
+	new_spp_sp = "mammals_water.shp") #, 
+	#new_spp_sf = "new_mammal_water_w_dat.rds")
 
 
 
