5d5eb8c8e9c9ef42797a7a2e24a4807806bb3bed
Sara Williams <sarawil@gmail.com>
2017-08-17 14:10:18 -0600

    Script for plot creation
    Create plots using ggplot from new spp range polygons - plot as sf objects and adjust transparency (via alpha) to see areas with more or less species. Create plots using rasters with each pixel value representing the number of spp ranges that overlap the pixel.

--- new_spp_range_plots.R
+++ new_spp_range_plots.R
@@ -1,26 +1,27 @@
 #  Sara Williams
-#  August 16, 2017
+#  August 16, 2017; August 17, 2017
 #  Script to plot adjusted species distribution polygons with 
 #  information on constraints such as elevation, forest, and water dependency.
 
 library(sf)
 library(sp)
-library(viridis)
 library(raster)
 library(tidyverse) 
 ####  NOTE: Must install the development version of ggpot2 to use geom_sf().
 ####   devtools::install_github("tidyverse/ggplot2")
 
 
-#  Load new spp range polygons
-mams_new <- shapefile("C:/Users/saraw/Documents/SEARRP/processed_spat_data/new_ranges/mammals_elev.shp")
-amphs_new <- shapefile("C:/Users/saraw/Documents/SEARRP/processed_spat_data/new_ranges/amphibians_elev.shp")
-birds_new <- shapefile("C:/Users/saraw/Documents/SEARRP/processed_spat_data/new_ranges/birds_elev.shp")
 
-#  Load other spp info
-mams_dat <- read.csv("C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_mammals_threatened.csv")
-amphs_dat <- read.csv("C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_amphibians_threatened.csv")
-birds_dat <- read.csv("C:/Users/saraw/Documents/SEARRP/processed_excel_data/sabah_birds_threatened.csv")
+#  Load new spp range polygons sf objects, SpatialPixelsDataFrames and rasters
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/new_mams_sf_dat.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/new_amphs_sf_dat.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/new_birds_sf_dat.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/pts_mams.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/pts_amphs.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/pts_ birds.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/mams_r.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/amphs_r.rds")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/maps/birds_r.rds")
 
 #  Load environmental/border data
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/log_rds_sf.Rdata")
@@ -28,43 +29,89 @@ load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/f
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sabah_d.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sarawak_d.Rdata")
 
-#  Convert to sf objects
-mams_sf <- st_as_sf(mams_new)
-amphs_sf <- st_as_sf(amphs_new)
-birds_sf <- st_as_sf(birds_new)
 
-sarawak_sf <- st_as_sf(border_sarawak_d)
+#  Function and data prep to use ggplot with RasterLayer
+raster_ggplot <- function(rastx) {
+  require(SDMTools)
+  stopifnot(class(rastx) == "RasterLayer")
+  gfx_data <- getXYcoords(rastx)
+  # lats need to be flipped
+  gfx_data <- expand.grid(lons = gfx_data$x, lats = rev(gfx_data$y), 
+                            stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
+  gfx_data$n_spp <- rastx@data@values
+  return (gfx_data)
+}
 
-#  Combine sf object with other spp info
-mams_sf_dat <- bind_cols(mams_sf, mams_dat)
-amphs_sf_dat <- bind_cols(amphs_sf, amphs_dat)
-birds_sf_dat <- bind_cols(birds_sf, birds_dat)
-
-
-
-  
-#  Rasterize the number of polygons overlapping each cell
-#   Make empty raster template and SPDF
-r_template <- raster(ncols = 1000, nrows = 1000, 
-   crs = projection(border_sabah_d), 
-   ext = extent(border_sabah_d))
-pts <- rasterToPoints(r_template, spatial = TRUE)
-proj4string(pts) = proj4string(mams_new) # otherwise over will fail here;
-pts$n = sapply(over(pts, geometry(mams_new), returnList = TRUE), length)
-gridded(pts) = TRUE
+#  Convert to data frame readily available for ggplot2
+mams_spp_gg <- raster_ggplot(mams_r)
+amphs_spp_gg <- raster_ggplot(amphs_r)
+birds_spp_gg <- raster_ggplot(birds_r)
 
 
-spplot(pts["n"], sp.layout=list("sp.polygons", mams_new, first=F))
 
 
-mams_r <- rasterize(mams_new, r_template, fun = 'count')
+
+mams_r_p <- ggplot(mams_spp_gg) +
+  geom_raster(aes(lons, lats, fill = n_spp)) +
+  #geom_sf(data = log_rds_sf, colour = "darkgrey") +
+  #coord_sf(crs = st_crs(32650)) +
+  scale_fill_distiller(palette = "Spectral", name = "Number of spp") +
+  #scale_fill_gradient(low = "white", high = "red", guide = "colourbar") +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Mammals - Threatened Species Ranges") +
+  theme_bw()
+mams_r_p
+
+
+
+
+
  
-#  Plot
+#  Plots using polygons and alpha (transparency)
 mams_p <- ggplot(mams_sf_dat) +
-  geom_sf(colour = "transparent", fill = "darkorange", alpha = 0.02) +
+  geom_sf(colour = "transparent", fill = "darkred", alpha = 0.018) +
   #geom_sf(data = log_rds_sf, colour = "darkgrey") +
   coord_sf(crs = st_crs(32650)) +
   xlab("Latitude") +
   ylab("Longitude") +
+  ggtitle("Mammals - Threatened Species Ranges") +
   theme_bw()
 mams_p
+
+amphs_p <- ggplot(amphs_sf_dat) +
+  geom_sf(colour = "transparent", fill = "darkred", alpha = 0.018) +
+  #geom_sf(data = log_rds_sf, colour = "darkgrey") +
+  coord_sf(crs = st_crs(32650)) +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Amphibians - Threatened Species Ranges") +
+  theme_bw()
+amphs_p
+
+birds_p <- ggplot(birds_sf_dat) +
+  geom_sf(colour = "transparent", fill = "darkred", alpha = 0.018) +
+  #geom_sf(data = log_rds_sf, colour = "darkgrey") +
+  coord_sf(crs = st_crs(32650)) +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Birds - Threatened Species Ranges") +
+  theme_bw()
+birds_p
+
+
+
+
+# #  Plot using raster converted to sf object
+# pts_mams_sf <- st_as_sf(pts_mams)
+
+# mams_r_p <- ggplot(pts_mams_sf) +
+  # geom_sf(fill = n) +
+  # #geom_sf(data = log_rds_sf, colour = "darkgrey") +
+  # #coord_sf(crs = st_crs(32650)) +
+  # scale_fill_viridis("Number of species present") +
+  # xlab("Latitude") +
+  # ylab("Longitude") +
+  # ggtitle("Mammals - Threatened Species Ranges") +
+  # theme_bw()
+# mams_r_p
