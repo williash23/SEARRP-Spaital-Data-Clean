3c9f87a0c4faf49967b38ec7e9f76fce1d46f196
Sara Williams <sarawil@gmail.com>
2017-08-17 16:15:55 -0600

    Adding rasterVis plots
    Adding script to plot rasters with spp number per pixel using rasterVis. Much nicer visualization.

--- new_spp_range_plots.R
+++ new_spp_range_plots.R
@@ -7,6 +7,8 @@ library(sf)
 library(sp)
 library(raster)
 library(tidyverse) 
+library(rasterVis)
+library(RColorBrewer)
 ####  NOTE: Must install the development version of ggpot2 to use geom_sf().
 ####   devtools::install_github("tidyverse/ggplot2")
 
@@ -28,41 +30,39 @@ load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/l
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/for_cov_c.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sabah_d.Rdata")
 load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_sarawak_d.Rdata")
+load(file="C:/Users/saraw/Documents/SEARRP/processed_spat_data/trans_crop_proj/border_kali_d.Rdata")
 
 
-#  Function and data prep to use ggplot with RasterLayer
-raster_ggplot <- function(rastx) {
-  require(SDMTools)
-  stopifnot(class(rastx) == "RasterLayer")
-  gfx_data <- getXYcoords(rastx)
-  # lats need to be flipped
-  gfx_data <- expand.grid(lons = gfx_data$x, lats = rev(gfx_data$y), 
-                            stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
-  gfx_data$n_spp <- rastx@data@values
-  return (gfx_data)
-}
+#  Use rasterVis package
+#   Set color palette theme
+r_theme <- rasterTheme(rev(brewer.pal(8, "Spectral")))
 
-#  Convert to data frame readily available for ggplot2
-mams_spp_gg <- raster_ggplot(mams_r)
-amphs_spp_gg <- raster_ggplot(amphs_r)
-birds_spp_gg <- raster_ggplot(birds_r)
+plot_mams_r <- levelplot(mams_r, par.settings = r_theme, 
+	main = "Threated mammal species ranges",
+	xlab= "Longitude (UTM)",
+	ylab="Latitude (UTM)") +
+	layer(sp.polygons(border_sabah_d, lwd = 0.8, col = 'grey40')) + 
+	layer(sp.polygons(border_sarawak_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) +
+	layer(sp.polygons(border_kali_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) 
+plot_mams_r
 
+plot_amphs_r <- levelplot(amphs_r, par.settings = r_theme, 
+	main = "Threated amphibian species ranges",
+	xlab= "Longitude (UTM)",
+	ylab="Latitude (UTM)") +
+	layer(sp.polygons(border_sabah_d, lwd = 0.8, col = 'grey40')) + 
+	layer(sp.polygons(border_sarawak_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) +
+	layer(sp.polygons(border_kali_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) 
+plot_amphs_r
 
-
-
-
-mams_r_p <- ggplot(mams_spp_gg) +
-  geom_raster(aes(lons, lats, fill = n_spp)) +
-  #geom_sf(data = log_rds_sf, colour = "darkgrey") +
-  #coord_sf(crs = st_crs(32650)) +
-  scale_fill_distiller(palette = "Spectral", name = "Number of spp") +
-  #scale_fill_gradient(low = "white", high = "red", guide = "colourbar") +
-  xlab("Latitude") +
-  ylab("Longitude") +
-  ggtitle("Mammals - Threatened Species Ranges") +
-  theme_bw()
-mams_r_p
-
+plot_birds_r <- levelplot(birds_r, par.settings = r_theme, 
+	main = "Threated bird species ranges",
+	xlab= "Longitude (UTM)",
+	ylab="Latitude (UTM)") +
+	layer(sp.polygons(border_sabah_d, lwd = 0.8, col = 'grey40')) + 
+	layer(sp.polygons(border_sarawak_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) +
+	layer(sp.polygons(border_kali_d, lwd = 0.8, col = 'grey40', fill = 'gray40')) 
+plot_birds_r
 
 
 
@@ -102,16 +102,52 @@ birds_p
 
 
 
-# #  Plot using raster converted to sf object
-# pts_mams_sf <- st_as_sf(pts_mams)
 
-# mams_r_p <- ggplot(pts_mams_sf) +
-  # geom_sf(fill = n) +
-  # #geom_sf(data = log_rds_sf, colour = "darkgrey") +
-  # #coord_sf(crs = st_crs(32650)) +
-  # scale_fill_viridis("Number of species present") +
-  # xlab("Latitude") +
-  # ylab("Longitude") +
-  # ggtitle("Mammals - Threatened Species Ranges") +
-  # theme_bw()
-# mams_r_p
+#  Function and data prep to use ggplot with RasterLayer
+raster_ggplot <- function(rastx) {
+  require(SDMTools)
+  stopifnot(class(rastx) == "RasterLayer")
+  gfx_data <- getXYcoords(rastx)
+  # lats need to be flipped
+  gfx_data <- expand.grid(lons = gfx_data$x, lats = rev(gfx_data$y), 
+                            stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
+  gfx_data$n_spp <- rastx@data@values
+  return (gfx_data)
+}
+
+#  Convert to data frame readily available for ggplot2
+mams_spp_gg <- raster_ggplot(mams_r)
+amphs_spp_gg <- raster_ggplot(amphs_r)
+birds_spp_gg <- raster_ggplot(birds_r)
+   
+#  Plot raster-like objects
+mams_r_p <- ggplot(mams_spp_gg) +
+  geom_raster(aes(lons, lats, fill = n_spp)) +
+  coord_equal() +
+  geom_polygon(data = fortify(border_sabah_d), aes(long, lat, group=group)) +
+  scale_fill_distiller(palette = "Spectral", name = "Number of spp") +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Mammals - Threatened Species Ranges") +
+  theme_bw()
+mams_r_p
+
+
+amphs_r_p <- ggplot() +
+  geom_raster(data = amphs_spp_gg, aes(lons, lats, fill = n_spp)) +
+  scale_fill_distiller(palette = "Spectral", name = "Number of spp") +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Amphibians - Threatened Species Ranges") +
+  theme_bw()
+amphs_r_p 
+
+birds_r_p <- ggplot(birds_spp_gg) +
+  geom_raster(aes(lons, lats, fill = n_spp)) +
+  scale_fill_distiller(palette = "Spectral", name = "Number of spp") +
+  xlab("Latitude") +
+  ylab("Longitude") +
+  ggtitle("Birds - Threatened Species Ranges") +
+  theme_bw()
+birds_r_p
+
